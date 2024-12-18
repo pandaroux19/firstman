3b009756a4a32debe7f692f16223e8153ca57d65
Maison Remy <remy_maison@ens.univ-artois.fr>
2024-12-18 13:40:13 +0100

    oui

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    AOS.init({
+        offset: 300,     
+        delay: 1000,     
+        duration: 400,   
+        easing: 'ease',  
+        once: false,    
+        mirror: true     
+    });
+
+});
